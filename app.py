2c85a870e97072943a9fd5f13798195999c9b546
Daithí <davecoll3@gmail.com>
2023-03-10 11:02:01 +0000

    Add session user profile

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def sign_up():
         # put the new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("You have successfully signed up!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("sign-up.html")
 
 
@@ -60,8 +61,10 @@ def login():
             if check_password_hash(	
                 existing_user["password"], request.form.get("password")):	
                     session["user"] = request.form.get("username").lower()	
-                    flash("Welcome, {}".format(request.form.get("username")))	
-                    return redirect(url_for("login", username=session["user"]))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))	
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -73,6 +76,18 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # take the session user's username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+    
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
