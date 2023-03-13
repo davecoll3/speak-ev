2939bcc56cea9a98c3af0fe27a169ab842c1aca6
Daithí <davecoll3@gmail.com>
2023-03-13 16:23:27 +0000

    Wired up delete button to delete terms from the database

--- app.py
+++ app.py
@@ -137,6 +137,13 @@ def edit_term(term_id):
     return render_template("edit_term.html", term=term, categories=categories)
 
 
+@app.route("/delete_term/<term_id>")
+def delete_term(term_id):
+    mongo.db.terms.delete_one({"_id": ObjectId(term_id)})
+    flash("Term Successfully Deleted fom Dictionary")
+    return redirect(url_for("get_terms"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
