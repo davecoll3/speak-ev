d6d9c4ec64e5ec9d69295ff8add9b690d0b50cd4
Daithí <davecoll3@gmail.com>
2023-03-13 12:49:52 +0000

    Add Materialize select validation

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-/* mobile sidenav and collapsible sourced from Materialize: https://materializecss.com/ */
+/* mobile sidenav, collapsible, and selects dropdown  code sourced from Materialize: https://materializecss.com/ */
 document.addEventListener('DOMContentLoaded', function() {
 let sidenavs = document.querySelectorAll(".sidenav");
 let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
@@ -6,4 +6,42 @@ let collapsibles = document.querySelectorAll(".collapsible");
 let collapsiblesInstance = M.Collapsible.init(collapsibles);
 let selects = document.querySelectorAll("select");
 let selectsInstance = M.FormSelect.init(selects);
+
+    /* Materialize select validation code sourced from Code Institute: https://bit.ly/3mMOpsi */
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {         
+        let classValid = "border-bottom: 1px solid #4caf50; box-shadow: 0 1px 0 0 #4caf50;";
+        let classInvalid = "border-bottom: 1px solid #f44336; box-shadow: 0 1px 0 0 #f44336;";
+        let selectValidate = document.querySelector("select.validate");
+        let selectWrapperInput = document.querySelector(".select-wrapper input.select-dropdown");
+        if (selectValidate.hasAttribute("required")) {
+            selectValidate.style.cssText = "display: block; height: 0; padding: 0; width: 0; position: absolute;";
+        }
+        selectWrapperInput.addEventListener("focusin", (e) => {
+            e.target.parentNode.addEventListener("change", () => {
+                ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+                for (let i = 0; i < ulSelectOptions.length; i++) {
+                    if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled") {
+                        e.target.style.cssText = classValid;
+                    }
+                }
+            });
+        });
+        selectWrapperInput.addEventListener("click", (e) => {
+            ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+            for (let i = 0; i < ulSelectOptions.length; i++) {
+                if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled" && ulSelectOptions[i].style.backgroundColor == "rgba(0, 0, 0, 0.03)") {
+                    e.target.style.cssText = classValid;
+                } else {
+                    e.target.addEventListener("focusout", () => {
+                        if (e.target.parentNode.childNodes[3].hasAttribute("required")) {
+                            if (e.target.style.borderBottom != "1px solid rgb(76, 175, 80)") {
+                                e.target.style.cssText = classInvalid;
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
 });
