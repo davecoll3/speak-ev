f7d7aa80371993a159ecec7ab7a6c507a52734b0
Daithí <davecoll3@gmail.com>
2023-06-12 15:04:30 +0000

    Build: set debug to false

--- app.py
+++ app.py
@@ -292,4 +292,4 @@ def dislike(term_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
